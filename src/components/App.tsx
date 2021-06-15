0550e1d42605417e1b8927194cfbd388223d64d2
Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-06-15 23:46:15 +0300

    setup settings

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledAppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 22px;
+`;
+
+const App: React.FC = () => {
+  return <StyledAppContainer>Work in progress</StyledAppContainer>;
+};
+
+export default App;
